23acb8470ccab5d6b0af1b3b4232765555bc10b2
CereuslyDilutedScience <marciakcoburn@gmail.com>
2025-12-29 14:26:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -86,8 +86,8 @@ function renderPages(pages, viewer) {
         const span = document.createElement("span");
         span.className = "word";
         span.textContent = word.term || word.text;
-span.style.color = "transparent";        // hides the letters
-span.style.backgroundColor = "rgba(255, 255, 0, 0.3)";  // keeps highlight visible
+span.style.color = "rgba(255, 255, 0, 0)";        // hides the letters
+span.style.backgroundColor = "rgba(255, 255, 0, 0)";  // keeps highlight visible
 
 
         if (word.definition) {
